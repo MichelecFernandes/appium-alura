9b4c3e9ade9ced9a3e68791956867099534b5ef3
MichelecFernandes <michelecristinafr@gmail.com>
2024-08-01 14:15:58 -0300

    feat: adicao da pasta tools que estava obsoleta

--- src/main/java/com/alura/appium/App.java
+++ src/main/java/com/alura/appium/App.java
@@ -1,13 +1,11 @@
 package com.alura.appium;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.net.MalformedURLException;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args) throws MalformedURLException{
+
     }
+
 }
---
+++ src/test/java/com/alura/appium/FeatureCadastro.java
@@ -0,0 +1,18 @@
+package com.alura.appium;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+
+
+public class FeatureCadastro
+{
+
+    @Test
+    public void cadastroComSenhasDiferentes() throws MalformedURLException {
+        AppiumDriverConfig driver = new AppiumDriverConfig();
+        assertTrue( true );
+    }
+}
